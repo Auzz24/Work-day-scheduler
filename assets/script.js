a47c46bbe7816f631b69f9d7062a18b6b9a7926f
Austin Miller <austin.miller1424@gmail.com>
2021-12-15 10:50:04 -0600

    editable tasks

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
 $( function() {
-  $( "#sortable" ).sortable();
+  $( "#sortable" ).sortable({
+    placeholder: "ui-state-height"
+  });
+  $("#sortable").disableSelection();
 } );
+
+
+
+
+//ATTEMPTS AT TEXT CHANGE 
+
+//document.getElementById('infoText2').innerHTML += document.getElementById('myText2').isContentEditable;
+
+// $(".list-group").on("click", "li", function() {
+//   //get cuttent text of li element
+//   var text = $(this)
+//     .text()
+//     .trim();
+
+//     //replace p element with a new textera
+//     var textInput = $("<textarea>").addclass("form-control").val(text);
+//     $(this).replacewith(textInput)
+
+//     textInput.trigger("focus");
+// });
+
